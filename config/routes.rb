551470320db796571e73a4037b65b26cd5a07ef4
github username <github email address>
2018-09-24 16:49:51 -0400

    rails generators code along.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  resources :accounts
+  get 'accounts/new'
+
+  get 'accounts/create'
+
+  get 'accounts/edit'
+
+  get 'accounts/update'
+
+  get 'accounts/destroy'
+
+  get 'accounts/index'
+
+  get 'accounts/show'
+
   get 'categories/show'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
